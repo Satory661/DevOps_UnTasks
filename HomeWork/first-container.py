69981b86058d069209634ba5e72480861080b8e4
Satory661 <gafurovelizar@gmail.com>
2024-10-26 13:09:15 +0500

    vse robit zashitil 3 labki

--- HomeWork/first-container.py
+++ HomeWork/first-container.py
@@ -1,1 +1,17 @@
-print("Неужели работает?")
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class RequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        
+        message = "Неужели работает?".encode("utf-8")
+        self.wfile.write(message)
+
+host = "0.0.0.0"
+port = 8000
+
+with HTTPServer((host, port), RequestHandler) as httpd:
+    print(f"Server running on port {port}...")
+    httpd.serve_forever()
